af3bd0a783728392ec4a159138491ed8b709d393
lucas.anacleto <lucas.anacleto@outlook.com>
2023-08-13 03:31:14 -0300

    initial car catalogue

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import { Hero } from '@/components'
+import { CustomFilter, Hero, SearchBar } from '@/components'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-contaner">
+          <h1 className="text-4xl font-extralight">
+            Catalogo de Carros
+          </h1>
+          <p>
+            Explore os carros que você pode gostar
+          </p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="combustível" />
+            <CustomFilter title="ano" />
+
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
